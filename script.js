7d9cf204ed7ec5d5a89011e24d8ee4445e3ca592
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2024-01-28 16:50:12 +0100

    add links eventListeners

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 // Getting the DOM elements
 const addItemLink = document.getElementById("add-item-link")
 const manageItemLink = document.getElementById("manage-item-link")
+const dashLink = document.getElementById("dash-link")
 const addItemPage = document.getElementById("add-item-page")
 const manageItemPage = document.getElementById("manage-item-page")
+const dashPage = document.getElementById("dash-page")
 const itemName = document.getElementById("item-name")
 const category = document.getElementById("category")
 const color = document.getElementById("color")
 const submitItem = document.getElementById("submit-item")
 const tableBody = document.getElementById("table-body")
+const allLinks = document.querySelectorAll("#inventory-div > div")
+
+const hideAllLinks = () => {
+    let linkArray = Array.from(allLinks)
+    linkArray.forEach(item => item.classList.add("hide"))
+}
+addItemLink.addEventListener("click", () =>{
+    hideAllLinks()
+    addItemPage.classList.remove("hide")
+})
+manageItemLink.addEventListener("click", () =>{
+    hideAllLinks()
+    manageItemPage.classList.remove("hide")
+})
+dashLink.addEventListener("click", () =>{
+    hideAllLinks()
+    dashPage.classList.remove("hide")
+})
